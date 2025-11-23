940212424959efd1b504efa6875cf4974d6a0c65
CapitanuAndreea <andreeacapitanu13@gmail.com>
2025-11-23 18:49:16 +0200

    Initial commit - Setup Configured

---
+++ Synesthesia.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Synesthesia.Web.Models;
+
+namespace Synesthesia.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {}
+
+        public DbSet<AudioFile> AudioFiles { get; set; }
+        public DbSet<SavedVideo> SavedVideos { get; set; }
+    }
+}
---
+++ Synesthesia.Web/Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Synesthesia.Web.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string? ProfilePicture { get; set; }
+        public string? Bio { get; set; }
+
+        // Relationships 1-M
+        public virtual ICollection<AudioFile>? AudioFiles { get; set; }
+        public virtual ICollection<SavedVideo>? SavedVideos { get; set; }
+    }
+}
