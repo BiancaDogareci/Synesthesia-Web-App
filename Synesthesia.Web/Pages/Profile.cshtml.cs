f610f88b9a7f11f655135190e3393c4510e5efe6
BiancaDogareci <biancadogareci@ymail.com>
2025-11-25 00:58:52 +0200

    Profile page

--- Synesthesia.Web/Pages/Profile.cshtml.cs
+++ Synesthesia.Web/Pages/Profile.cshtml.cs
@@ -1,12 +1,31 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Synesthesia.Web.Models;
 
 namespace Synesthesia.Web.Pages
 {
     public class ProfileModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<AppUser> _userManager;
+
+        public ProfileModel(UserManager<AppUser> userManager)
         {
+            _userManager = userManager;
+        }
+
+        public string? ProfilePicture { get; set; }
+        public string? Username { get; set; }
+        public string? Bio { get; set; }
+
+        public async Task OnGet()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user != null)
+            {
+                Username = user.UserName;
+                ProfilePicture = user.ProfilePicture;
+                Bio = user.Bio;
+            }
         }
     }
 }
